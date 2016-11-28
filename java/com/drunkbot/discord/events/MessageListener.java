0e005764f4dafdce5abaa98ea49b118db4c1dfd1
Dylan <dylan314@optonline.net>
2016-11-28 09:59:26 -0500

    More updates, moved text listener

--- java/com/drunkbot/discord/events/MessageListener.java
+++ java/com/drunkbot/discord/events/MessageListener.java
@@ -22,7 +22,7 @@ public class MessageListener implements IListener<MessageReceivedEvent> {
                 message.getChannel().sendMessage("ping");
             } else if (message.getContent().contains("SodiumCh1oride") || message.getMentions().get(0).getName().equalsIgnoreCase("SodiumCh1oride")) {
                 message.reply("That dude is a fag");
-            } else if (message.getAuthor().getName().equalsIgnoreCase("sodiumch1oride")){
+            } else if (message.getAuthor().getName().equalsIgnoreCase("sodiumch1oride")) {
                 int ran = randomGenerator.nextInt(100);
                 switch (ran % 4) {
                     case 0:
@@ -41,15 +41,9 @@ public class MessageListener implements IListener<MessageReceivedEvent> {
                         message.reply("Fuck off butt pirate");
                         break;
                 }
-            } else if ((message.getAuthor().getID().equalsIgnoreCase("222431757826392065") || message.getAuthor().getID().equalsIgnoreCase("193466098815991809" )) && message.getContent().toLowerCase().contains("timeout")) {
-                message.getGuild().getUserByID("221377585215438848").moveToVoiceChannel(message.getGuild().getVoiceChannelByID("250414798376927232"));
-                message.getChannel().sendMessage("Sending " +  message.getGuild().getUserByID("221377585215438848").getName() + "to the timeout channel");
-            } else if (message.getContent().equalsIgnoreCase("help!")) {
-                message.getAuthor().getOrCreatePMChannel().sendMessage("Avaliable Commands:\nsummon - Summons me to voice channel\ndismiss - removes me from voice channel\nHelp! - sens PM of help messages\n ping/pong - pings and pongs all day\nplay/airhorn - plays airhorn in summoned voice channel\nthere are secret commands too....");
-                if (message.getAuthor().getID().equalsIgnoreCase("193466098815991809")) {
-                    message.getAuthor().getOrCreatePMChannel().sendMessage("timeout - sends warren to the timeout channel");
-                }
             }
+
+
         } catch (Exception e ) {
             DrunkBot.logger.error(ExceptionUtils.getMessage(e));
         }
