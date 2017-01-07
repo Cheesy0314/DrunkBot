4dd478e520e8917e25198aafd916592e4eecac76
Dylan <dylan314@optonline.net>
2017-01-06 20:26:15 -0500

    Added new user messenger

---
+++ java/com/drunkbot/discord/events/JoinListener.java
@@ -0,0 +1,29 @@
+package com.drunkbot.discord.events;
+
+import sx.blah.discord.api.events.IListener;
+import sx.blah.discord.handle.impl.events.UserJoinEvent;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.IUser;
+
+/**
+ * Created by Dylan on 1/6/2017.
+ */
+public class JoinListener implements IListener<UserJoinEvent> {
+    @Override
+    public void handle(UserJoinEvent userJoinEvent) {
+        IUser newUser = userJoinEvent.getUser();
+        IGuild guild = userJoinEvent.getGuild();
+
+        try {
+            for (IChannel channel :guild.getChannels()) {
+                if (channel.getUsersHere().contains(newUser)) {
+                    channel.sendMessage("@" + newUser.getName() + ", welcome to " + guild.getName() + " discord server. Please take a few moments to read the rules.").addReaction(guild.getEmojiByName("gitgud"));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
